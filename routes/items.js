17d29153b9c87ce501d3016182275ea114b95be1
wyh20240819 <wangyonghong@byaidata.com>
2024-10-25 17:22:43 +0800

    项目管理

--- routes/items.js
+++ routes/items.js
@@ -2,11 +2,13 @@
  * @Description: 
  * @Author: wangyonghong
  * @Date: 2024-10-22 09:56:41
- * @LastEditTime: 2024-10-24 18:17:05
+ * @LastEditTime: 2024-10-25 16:46:36
  */
 const express = require('express');
 const moment = require('moment')
 const router = express.Router();
+const path = require('path');
+const { formidable } = require('formidable');
 const checkTokenMiddleware = require('../middlewares/tokenMiddlewares')
 const { query } = require('../util/dbconfig');
 
@@ -70,27 +72,27 @@ router.post('/item/add', checkTokenMiddleware, async (req, res) => {
   
 //项目管理-项目列表-编辑
 router.post('/item/edit', checkTokenMiddleware, async (req, res) => {
-const { edit_id,service_line,item,sex,age,specialty,education,certificate,language_competence,
-        ability,work_experience,model_experience,likes,characters } = req.body
-        
-const sql = `UPDATE items
-                SET service_line = '${service_line}', item = '${item}', sex = '${sex}',
-                age = '${age}', specialty = '${specialty}', education = '${education}',certificate = '${certificate}',
-                language_competence = '${language_competence}', ability = '${ability}', work_experience = '${work_experience}', 
-                model_experience = '${model_experience}',likes = '${likes}',characters = '${characters}'
-                WHERE id = ${edit_id}`
-let dataList = await query( sql ) 
-if(dataList){
-    res.json({
-    status:1,
-    msg:'请求成功...',
-    })
-}else{
-    res.json({
-    status:0,
-    msg:'请求失败...',
-    })
-}
+    const { edit_id,service_line,item,sex,age,specialty,education,certificate,language_competence,
+            ability,work_experience,model_experience,likes,characters } = req.body
+            
+    const sql = `UPDATE items
+                    SET service_line = '${service_line}', item = '${item}', sex = '${sex}',
+                    age = '${age}', specialty = '${specialty}', education = '${education}',certificate = '${certificate}',
+                    language_competence = '${language_competence}', ability = '${ability}', work_experience = '${work_experience}', 
+                    model_experience = '${model_experience}',likes = '${likes}',characters = '${characters}'
+                    WHERE id = ${edit_id}`
+    let dataList = await query( sql ) 
+    if(dataList){
+        res.json({
+        status:1,
+        msg:'请求成功...',
+        })
+    }else{
+        res.json({
+        status:0,
+        msg:'请求失败...',
+        })
+    }
 });
 
 //项目管理-项目列表-删除
@@ -143,15 +145,14 @@ router.get('/account/search', checkTokenMiddleware, async (req, res) => {
 
 //项目管理-对账列表-新增
 router.post('/account/add', checkTokenMiddleware, async (req, res) => {
-  const { id,name,service_line,base,project_leader,status,
-          settlement_type,settlement_day,day,start_date,delivery_date } = req.body
+  const { id,name,service_line,base,project_leader,status,settlement_type,settlement_day,
+          day,start_date,delivery_date } = req.body
   const time = moment().format('YYYY-MM-DD HH:mm:ss')
-
   const sql = `insert into account(item_id,item_name,service_line,base,
-               project_leader,item_status,item_day,item_settlement_type,
-               item_settlement_day,item_start_date,item_delivery_date,create_time)
-               VALUES('${id}','${name}','${service_line}','${base}','${project_leader}','${status}',
-               '${day}','${settlement_type}','${settlement_day}','${start_date}','${delivery_date}','${time}')`
+          project_leader,item_status,item_day,item_settlement_type,
+          item_settlement_day,item_start_date,item_delivery_date,create_time)
+          VALUES('${id}','${name}','${service_line}','${base}','${project_leader}','${status}',
+          '${day}','${settlement_type}','${settlement_day}','${start_date}','${delivery_date}','${time}')`
   let dataList = await query( sql ) 
   if(dataList){
     res.json({
@@ -168,7 +169,7 @@ router.post('/account/add', checkTokenMiddleware, async (req, res) => {
 
 //项目管理-对账列表-明细-查询
 router.get('/account/detail', checkTokenMiddleware, async (req, res) => {
-  const sql = `select * from account_detail`
+  const sql = `select * from account_detail where account_id ='${req.query.id}'`
   let dataList = await query( sql ) 
   if(dataList){
     res.json({
@@ -186,18 +187,30 @@ router.get('/account/detail', checkTokenMiddleware, async (req, res) => {
 
 //项目管理-对账列表-明细-新增
 router.post('/account/detail_add', checkTokenMiddleware, async (req, res) => {
-  const { account_id,account_day,account_period,tasks,settlement_scale,
-          amount,price,sum,is_accept } = req.body
+  const { item_settlement_type,account_id,account_day,account_period,tasks,settlement_scale,
+          amount,price,sum,is_accept,normal_hour,normal_overtime_hour,week_overtime_hour,
+          holidays_overtime_hour,times_overtime_hour15,times_overtime_hour2,times_overtime_hour3} = req.body
   const time = moment().format('YYYY-MM-DD HH:mm:ss')
   const _account_day = moment(account_day).format('YYYY-MM-DD')
   let _account_period_0 = moment(account_period[0]).format('YYYY-MM-DD')
   let _account_period_1 = moment(account_period[1]).format('YYYY-MM-DD')
   let _account_period   = _account_period_0 + '至' + _account_period_1
   const user = req.user.account
-  const sql = `insert into account_detail(account_id,reconciler,account_day,account_period,tasks,
-               settlement_scale,amount,price,sum,is_accept,create_time)
-               VALUES('${account_id}','${user}','${_account_day}','${_account_period}','${tasks}',
-               '${settlement_scale}','${amount}','${price}','${sum}','${is_accept}','${time}')`
+  let sql 
+  if(item_settlement_type === '计件'){
+      sql = `insert into account_detail(account_id,reconciler,account_day,account_period,tasks,
+              settlement_scale,amount,price,sum,is_accept,create_time)
+              VALUES('${account_id}','${user}','${_account_day}','${_account_period}','${tasks}',
+              '${settlement_scale}','${amount}','${price}','${sum}','${is_accept}','${time}')`
+  }else{
+      sql = `insert into account_detail(account_id,reconciler,account_day,account_period,tasks,
+             settlement_scale,normal_hour,normal_overtime_hour,week_overtime_hour,holidays_overtime_hour,
+             times_overtime_hour15,times_overtime_hour2,times_overtime_hour3,price,sum,is_accept,create_time)
+             VALUES('${account_id}','${user}','${_account_day}','${_account_period}','${tasks}',
+             '${settlement_scale}','${normal_hour}','${normal_overtime_hour}','${week_overtime_hour}',
+             '${holidays_overtime_hour}','${times_overtime_hour15}','${times_overtime_hour2}','${times_overtime_hour3}',
+             '${price}','${sum}','${is_accept}','${time}')`
+  }
   let dataList = await query( sql ) 
   if(dataList){
     res.json({
@@ -213,7 +226,8 @@ router.post('/account/detail_add', checkTokenMiddleware, async (req, res) => {
 });
 
 //项目管理-对账列表-附件上传
-router.post('/account/upload', checkTokenMiddleware, (req, res) => {
+router.post('/account/detail/upload', checkTokenMiddleware, (req, res) => {
+  let id = req.query.id
   //创建form对象
   const form = formidable({
     multiples:true,
@@ -229,33 +243,20 @@ router.post('/account/upload', checkTokenMiddleware, (req, res) => {
        })
       return;
     }
-    let originalFilename = files.file[0].originalFilename
-    let type = path.extname(originalFilename).slice(1)
-    if(type != 'xlsx'){
+    let fileUrl = '/excel/' + files.file[0].newFilename
+    let sql = `UPDATE account_detail SET attachment='${fileUrl}' where id='${id}'`
+    let dataList = await query( sql ) 
+    if(dataList){
       res.json({
-        status:3,
-        msg:'文件类型错误',
-       })
-       return;
+        status:1,
+        msg:'请求成功...',
+      })
+    }else{
+      res.json({
+        status:0,
+        msg:'请求失败...',
+      })
     }
-
-    let fileUrl = '../public/excel/' + files.file[0].newFilename
-    //如果是图片、保存url:/excel/' + files.file[0].newFilename
-    const filePath = path.join(__dirname, fileUrl);
-
-      let sql = `INSERT INTO xxx() VALUES ()`
-      let dataList = await query( sql ) 
-      if(dataList){
-        res.json({
-          status:1,
-          msg:'请求成功...',
-        })
-      }else{
-        res.json({
-          status:0,
-          msg:'请求失败...',
-        })
-      }
   });
 });
 
