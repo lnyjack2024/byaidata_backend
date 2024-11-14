86108efafa498458d2e9fd8816dd4f771efc9ab7
wyh20240819 <wangyonghong@byaidata.com>
2024-11-14 15:36:49 +0800

    连接阿里云rds

--- util/dbconfig.js
+++ util/dbconfig.js
@@ -2,15 +2,20 @@
  * @Description: 数据库配置信息
  * @Author: wangyonghong
  * @Date: 2024-09-02 14:57:18
- * @LastEditTime: 2024-10-18 17:28:22
+ * @LastEditTime: 2024-11-14 15:36:16
  */
 const mysql = require('mysql')
 const pool = mysql.createPool({
-//   connectionLimit: 50,
-    host: 'localhost',
-    user: 'root',
-    password: 'wyhmysql',
-    database: 'by_mysql'
+    connectionLimit: 50,
+    host: 'rm-uf6a7ab0tn1139ug9.mysql.rds.aliyuncs.com',//外网地址 ecs连接内网地址、设置白名单
+    user: 'test_root',
+    password: 'Byaidata@2024',
+    database: 'byaidata_database_test',
+    port: 3306,
+    connectTimeout: 10000,      // 连接超时时间
+    waitForConnections: true,   // 连接等待
+    acquireTimeout: 10000,      // 获取连接的超时时间
+    queueLimit: 0               // 排队等待连接的数量（0 表示不限制）
 }) 
 let query = function( sql, values ) { 
     return new Promise(( resolve, reject ) => { 
