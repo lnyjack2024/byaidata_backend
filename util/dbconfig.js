55e11e390960a2790ad37dc2915ea21bdc283e0c
wyh20240819 <wangyonghong@byaidata.com>
2024-12-23 15:58:39 +0800

    生产环境连接rds配置信息

--- util/dbconfig.js
+++ util/dbconfig.js
@@ -2,15 +2,19 @@
  * @Description: 数据库配置信息
  * @Author: wangyonghong
  * @Date: 2024-09-02 14:57:18
- * @LastEditTime: 2024-12-23 13:07:18
+ * @LastEditTime: 2024-12-23 13:22:02
  */
 const mysql = require('mysql')
 const pool = mysql.createPool({
     connectionLimit: 50,
-    host: 'rm-uf6a7ab0tn1139ug9.mysql.rds.aliyuncs.com',//外网地址 ecs连接内网地址、设置白名单
-    user: 'test_root',
-    password: 'Byaidata@2024',
-    database: 'byaidata_database_test',
+    // host: 'rm-uf6a7ab0tn1139ug9.mysql.rds.aliyuncs.com',//外网地址 ecs连接内网地址、设置白名单
+    // user: 'test_root',
+    // password: 'Byaidata@2024',
+    // database: 'byaidata_database_test',
+    host: 'rm-uf68irq87i3de22q1.mysql.rds.aliyuncs.com',//外网地址 ecs连接内网地址、设置白名单
+    user: 'pro_root',
+    password: 'ByaidataPro@2024',
+    database: 'byaidata_database_pro',
     port: 3306,
     connectTimeout: 10000,      // 连接超时时间
     waitForConnections: true,   // 连接等待
